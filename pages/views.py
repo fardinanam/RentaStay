b84e647330e353229a4c1362ab61bdeaa113b96a
Fardin Anam Aungon <52667920+fardinanam@users.noreply.github.com>
2022-02-03 12:07:14 +0600

    Home page reedited

--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.db import connection, IntegrityError
-
+from rentastay import definitions
 # Create your views here.
 
 def home(request):
     cursor = connection.cursor()
-    query = "SELECT * FROM HOUSES"
+    query = """SELECT HOUSE_NAME, CITY_NAME, STATE_NAME, COUNTRY_NAME
+                FROM HOUSES JOIN ADDRESSES USING(ADDRESS_ID) 
+                JOIN CITIES USING(CITY_ID) 
+                JOIN STATES USING(STATE_ID) 
+                JOIN COUNTRIES USING(COUNTRY_NAME)"""
     cursor.execute(query)
-    result = cursor.fetchall()
+    result = definitions.dictfetchall(cursor)
     cursor.close()
-
     data = {'houses':result}
     
     return render(request, 'pages/home.html', data)
